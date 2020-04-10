a3b0d3b3f6effb3b7c361d68b0d93483eab6cc19
Demetrio09 <demetrioalmeida@gmail.com>
2020-04-09 23:58:45 -0400

    Homework

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ var generateEl = document.getElementById("generate");
 
 // Setup variables with Arrays.
 
-var upperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM";
-var lowerCase = "zxcvbnmlkjhgfdsaqwertyuiop";
-var numbers = "0123456789";
-var symbols = "~!@#$%^&*()_+'='/*";
+var upperCaseEl = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+var lowerCaseEl = "zxcvbnmlkjhgfdsaqwertyuiop";
+var numbersEl = "0123456789";
+var symbolsEl = "~!@#$%^&*()_+'='/*";
 
 // Split the string to arrays.
 
-upperCase.split("");
-lowerCase.split("");
-numbers.split("");
-symbols.split("");
+upperCaseEl.split("");
+lowerCaseEl.split("");
+numbersEl.split("");
+symbolsEl.split("");
 
 // Prompt number of characters from user.
 
@@ -24,15 +24,28 @@ generateEl.addEventListener("click", () => {
 var passwordLenght = prompt("How many characters would you like?\nTip: It can be from 8 to 128 characters ;-)")
 
 if (passwordLenght >= 8 && passwordLenght <= 128) {
+    hasCharacters();
     return passwordLenght;
 }   
 else {
     alert("Please enter a number from 8 to 128.");
+    return "";
   } 
 });
 
 // Ask if user wants, Lower Case, Upper Case, Numbers and Symbols.
 
+pw = "";
+
 function hasCharacters() {
-    
+    var upperCase = confirm("Would you like to add upper case?");
+    var lowerCase = confirm("Would you like to add lower case?");
+    var numbers = confirm("Would you like to add numbers?");
+    var symbols = confirm("Would you like to add symbols?");
+
+    if (upperCase) {
+        pw.push(upperCaseEl.split(""));
+        console.log(pw);
+        
+    }
 }

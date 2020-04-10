c6283a194a5b4ea19360c8d4f8cb6ae5f2a3a92d
Demetrio09 <demetrioalmeida@gmail.com>
2020-04-10 12:46:20 -0400

    Homework

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ generateEl.addEventListener("click", () => {
 var passwordLenght = prompt("How many characters would you like?\nTip: It can be from 8 to 128 characters ;-)")
 
 if (passwordLenght >= 8 && passwordLenght <= 128) {
-    hasCharacters();
-    return passwordLenght;
+    hasCharacters(passwordLenght);
+   
 }   
 else {
     alert("Please enter a number from 8 to 128.");
@@ -33,19 +33,53 @@ else {
   } 
 });
 
-// Ask if user wants, Lower Case, Upper Case, Numbers and Symbols.
+// Ask user if they want Lower Case, Upper Case, Numbers and Symbols.
 
-pw = "";
+pw = [];
 
-function hasCharacters() {
+function hasCharacters(passwordLenght) {
     var upperCase = confirm("Would you like to add upper case?");
     var lowerCase = confirm("Would you like to add lower case?");
     var numbers = confirm("Would you like to add numbers?");
     var symbols = confirm("Would you like to add symbols?");
+    var all_options = [];
 
     if (upperCase) {
         pw.push(upperCaseEl.split(""));
-        console.log(pw);
-        
     }
+
+    if (lowerCase) {
+        pw.push(lowerCaseEl.split(""));
+    }
+
+    if (numbers) {
+        pw.push(numbersEl.split(""));
+    }
+
+    if (symbols) {
+        pw.push(symbolsEl.split(""));
+    }
+
+    for (let i = 0; i < pw.length; i++){
+        all_options.push(...pw[i])
+    }
+
+
+    
+    let shuffled = all_options
+    .map((a) => ({sort: Math.random(), value: a}));
+    .sort((a, b) => a.sort - b.sort);
+    .map((a) => a.value);
+
+   let final_random_password = [];
+
+   for (let i =0; i < passwordLenght;i++){
+
+    final_random_password.push(shuffled[i])
+   }
+
+   let user_password = final_random_password.join("");
+   pwEl.innerHTML = user_password;
+  
+
 }

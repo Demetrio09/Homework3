6107fdc679d02d7743076ccd308260d883c2bb8b
Demetrio09 <demetrioalmeida@gmail.com>
2020-04-09 23:42:00 -0400

    Homework

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-// Alert with welcome to the page!
-alert("Welcome to our Passworld Generator!");
+// Target the elements from the document.
 
-// Setup variables;
-    var needNewPassowrd = confirm("Do you need a New Passworld? OK for yes / CANCEL for no:");
+var pwEl = document.getElementById("password");
+var generateEl = document.getElementById("generate");
+
+// Setup variables with Arrays.
+
+var upperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+var lowerCase = "zxcvbnmlkjhgfdsaqwertyuiop";
+var numbers = "0123456789";
+var symbols = "~!@#$%^&*()_+'='/*";
+
+// Split the string to arrays.
+
+upperCase.split("");
+lowerCase.split("");
+numbers.split("");
+symbols.split("");
+
+// Prompt number of characters from user.
+
+generateEl.addEventListener("click", () => {
+
+var passwordLenght = prompt("How many characters would you like?\nTip: It can be from 8 to 128 characters ;-)")
+
+if (passwordLenght >= 8 && passwordLenght <= 128) {
+    return passwordLenght;
+}   
+else {
+    alert("Please enter a number from 8 to 128.");
+  } 
+});
+
+// Ask if user wants, Lower Case, Upper Case, Numbers and Symbols.
+
+function hasCharacters() {
     
-    var randomNumber = Math.floor(Math.random() * 9 ) + 1;
-    
-    var randomLetter8 = Math.random().toString(36).substring(2, 15);
-    var randomLetter12 = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-          
-// If and Else if statments collecting data from user;
-    if (needNewPassowrd) {
-        var eightCharacters = confirm("Click on OK to generate 8 characters or CANCEL to generate up 128 characters:");
-    }    
-    else {
-        alert("Came back anytime!");
-    }    
-    if (eightCharacters) {
-        var specialCharacter = confirm("Do you want to include lowercase, uppercase, numeric, and/or special characters");
-       
-    }
-    else {
-        var specialCharacter = confirm("Do you want to include lowercase, uppercase, numeric, and/or special characters");
-        
-    }
-    if (specialCharacter === true || specialCharacter === false) {
-        alert("Your new passworld is: " + randomLetter8);
-    }
-    else {
-        alert("Your new passworld is: " + randomLetter12);
-    }
-    
+}
